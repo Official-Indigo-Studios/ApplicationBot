0b31993a4d3e3a7580734adc17b719cc1755079a
TheBirmanator <birman.girl@att.net>
2020-03-23 16:01:34 -0700

    announce command

---
+++ commands/announce.js
@@ -0,0 +1,29 @@
+const { discord, client } = require('../index.js');
+
+module.exports = {
+    name: 'announce',
+    description: 'Have the bot make an announcement in a channel',
+
+    execute(message, args) {
+        if (args.length >= 3) {
+            var channelID = args.shift();
+            var channel = channelID.match(discord.MessageMentions.CHANNELS_PATTERN);
+            // found the channel ID
+            if (channel != null) {
+                channel = channel[0].slice(2, -1);
+                channel = message.guild.channels.cache.get(channel);
+                if (channel != undefined) {
+                    var announcement = args.join(' ');
+                    announcement = '@everyone\n' + announcement;
+                    channel.send(announcement);
+                } else {
+                    message.reply(`Invalid channel ${channelID} supplied!`);
+                }
+            } else {
+                message.reply(`Invalid channel ${channelID} supplied!`);
+            }
+        } else {
+            message.reply('that is too few arguments!');
+        }
+    }
+}
